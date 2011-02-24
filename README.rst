9ef2e250f5a6bedc2f6a97c14af76bd339af74d2
Dylan Jay <software@pretaweb.com>
2011-02-25 01:31:08 +1100

    initial commit

---
+++ README.rst
@@ -0,0 +1,66 @@
+XMLSource
+=========
+
+A simple transmogrifier blueprint to read xml files of the form ::
+
+    <container>
+      <item>
+        <field1>value</field1>
+        <field2>value</field2>
+        ...
+      </item>
+      ...
+    </container>
+
+
+Example
+-------
+
+The following example uses `funnelweb` and `ploneremote` to upload
+content in an xml file from the commandline
+
+First create a custom `pipeline.cfg` ::
+
+    [transmogrifier]
+    pipeline =
+        xmlsource
+        ploneupload
+        ploneupdate
+
+    [xmlsource]
+    blueprint = transmogrify.xmlsource
+    xmlfile = items.xml
+    pathtag = field1
+    itemtag = item
+    type = MyCustomType
+
+    [ploneupload]
+    blueprint = transmogrify.ploneremote.remoteconstructor
+    target = http://admin:admin@localhost/Plone/mycontent
+
+    [ploneupdate]
+    blueprint = transmogrify.ploneremote.remoteschemaupdater
+    target = ${ploneupload:target}
+
+Now install `funnelweb` using a `buildout.cfg` ::
+
+    [buildout]
+    parts = convertxml
+
+    [convertxml]
+    recipe = funnelweb
+    pipeline=wynhotels.cfg
+    eggs = transmogrify.xmlsource
+
+bootstrap ::
+
+    $> easy_install zc.buildout
+    $> buildout init
+    $> bin/buildout
+
+and now run your custom converter
+
+    $> bin/convertxml
+
+The converter parses the xml and uploads to plone via xmlrpc. or
+construct your own pipeline to transform the content into or whereever.
---
+++ transmogrifyxmlsource/__init__.py
@@ -0,0 +1,55 @@
+#
+
+from zope.interface import classProvides, implements
+from collective.transmogrifier.interfaces import ISectionBlueprint
+from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.utils import defaultMatcher
+from transmogrify.pathsorter.treeserializer import TreeSerializer
+
+from Acquisition import aq_base
+from Products.CMFCore.utils import getToolByName
+from urlparse import urlparse, urljoin
+import logging
+import lxml
+
+class XMLSource(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = TreeSerializer(transmogrifier, name, options, previous)
+        self.context = transmogrifier.context
+
+        self.xmlfile = options.get('xmlfile','')
+        self.path = options.get('path', '')
+        self.type = options.get('type', 'Document')
+        self.itemtag = options.get('itemtag')
+        self.pathtag = options.get('pathtag')
+        self.logger = logging.getLogger(name)
+
+    def __iter__(self):
+        for item in self.previous:
+            yield item
+
+        f = open(self.xmlfile, "r")
+        events = ("start", "end", "start-ns", "end-ns")
+        context = lxml.etree.iterparse(f, events=events)
+        item = {}
+        for action, elem in context:
+            if action in ('start') and elem.tag == self.itemtag:
+                item = {}
+            elif action == 'end' and elem.tag == self.itemtag:
+                item['_path'] = item[self.pathtag]
+                item['_type'] = self.type
+                self.logger.debug(str(item))
+                yield item
+                item = None
+            elif action == 'end' and item is not None:
+                #import pdb; pdb.set_trace()
+                item[elem.tag] = elem.text
+
+            
+
+
+
